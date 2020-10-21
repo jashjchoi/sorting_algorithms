d9c6d50db6bc91fa794a158d5850493035c91524
jashjchoi <jashjchoi@gmail.com>
2020-10-21 18:58:16 +0000

    Update the header file

--- sort.h
+++ sort.h
@@ -24,5 +24,7 @@ void swap_num(int *array, int i);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
-
+void qst(int array[], int low, int high, size_t size);
+int partition(int *array, int l, int h, size_t size);
+void swap_func(int *x, int *y);
 #endif /** SORT_H **/
