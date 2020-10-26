5d336ae21bc72f5d9d2510e48371a7aaa804e28d
jashjchoi <jashjchoi@gmail.com>
2020-10-26 00:06:38 +0000

    Add more prototype function

--- sort.h
+++ sort.h
@@ -22,6 +22,7 @@ void print_array(const int *array, size_t size);
 void bubble_sort(int *array, size_t size);
 void swap_num(int *array, int i);
 void insertion_sort_list(listint_t **list);
+void swap_int(listint_t *r, listint_t *l);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void qst(int array[], int low, int high, size_t size);
